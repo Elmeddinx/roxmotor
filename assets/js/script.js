63b029d0860405d8701bcca5b72fea778f157201
elmeddinx <xudaverdiyev11@gmail.com>
2024-07-08 00:06:02 +0400

    Finish section 4 & 5

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,137 +7,188 @@ var heroBtn = document.getElementById("heroBtn");
 var heroBgVideo = document.getElementById("heroBgVideo");
 
 headerLang.addEventListener("click", (e) => {
-  headerLangDropdown.classList.toggle("activeLang");
+    headerLangDropdown.classList.toggle("activeLang");
 });
 
 document.addEventListener("click", (e) => {
-  if (
-    !headerLang.contains(e.target) &&
-    !headerLangDropdown.contains(e.target)
-  ) {
-    headerLangDropdown.classList.remove("activeLang");
-  }
+    if (
+        !headerLang.contains(e.target) &&
+        !headerLangDropdown.contains(e.target)
+    ) {
+        headerLangDropdown.classList.remove("activeLang");
+    }
 });
 
 hamburgerMenuBtn.addEventListener("click", (e) => {
-  hamburgerMenu.classList.toggle("activeMenu");
+    hamburgerMenu.classList.toggle("activeMenu");
 });
 
 heroBtn.addEventListener("click", function () {
-  var videoPlayerElement = document.getElementById("heroVideoPlayer");
-  var currentTime = heroBgVideo.currentTime;
-  videoPlayerElement.style.display = "block";
-  player.currentTime(currentTime);
-  player.requestFullscreen();
-  player.play();
+    var videoPlayerElement = document.getElementById("heroVideoPlayer");
+    var currentTime = heroBgVideo.currentTime;
+    videoPlayerElement.style.display = "block";
+    player.currentTime(currentTime);
+    player.requestFullscreen();
+    player.play();
 });
 player.on("fullscreenchange", function () {
-  if (!player.isFullscreen()) {
-    player.pause();
-    player.currentTime();
-    document.getElementById("heroVideoPlayer").style.display = "none";
-  }
+    if (!player.isFullscreen()) {
+        player.pause();
+        player.currentTime();
+        document.getElementById("heroVideoPlayer").style.display = "none";
+    }
 });
 
 // Section1
 function createCarousel(carouselId, itemClass, captionClass, captionMbClass, captionTitleClass, captionDescClass, captionHrClass, mbTitleClass, mbDescClass) {
-  let currentIndex = 0;
-  let intervalId;
-  const carousel = document.getElementById(carouselId);
-  const slides = carousel.querySelectorAll(`.${itemClass}`);
-  const captions = carousel.querySelectorAll(`.${captionClass}`);
-  const captionsMb = carousel.querySelectorAll(`.${captionMbClass}`);
-  const activeCaptionMbTitle = carousel.querySelector(`.${mbTitleClass}`);
-  const activeCaptionMbDesc = carousel.querySelector(`.${mbDescClass}`);
+    let currentIndex = 0;
+    let intervalId;
+    const carousel = document.getElementById(carouselId);
+    const slides = carousel.querySelectorAll(`.${itemClass}`);
+    const captions = carousel.querySelectorAll(`.${captionClass}`);
+    const captionsMb = carousel.querySelectorAll(`.${captionMbClass}`);
+    const activeCaptionMbTitle = carousel.querySelector(`.${mbTitleClass}`);
+    const activeCaptionMbDesc = carousel.querySelector(`.${mbDescClass}`);
+    const captions3Mb = document.getElementById("captions3Mb");
+    const captions4Mb = document.getElementById("captions4Mb");
+    
+    document.getElementById("carousel3").style.display = "none";
+    document.getElementById("carousel4").style.display = "none";
+    
+    if (carousel == document.getElementById("carousel3") && captions3Mb && window.innerWidth <= 768) {
+        slides.forEach((slide, index) => {
+            console.log(slide)
+            const newItem = document.createElement('div');
+            newItem.classList.add(captionMbClass); 
 
-  function showSlide(index) {
-      if (index >= slides.length) {
-          currentIndex = 0;
-      } else if (index < 0) {
-          currentIndex = slides.length - 1;
-      } else {
-          currentIndex = index;
-      }
+            const titleElement = document.createElement('h4');
+            titleElement.classList.add(mbTitleClass);
+            titleElement.innerHTML = captions[index].querySelector(`.${captionTitleClass}`).innerHTML;
+            newItem.appendChild(titleElement);
 
-      slides.forEach((slide) => slide.classList.remove("active"));
-      captionsMb.forEach((slide) => slide.classList.remove("active"));
+            let mediaElement = slide.querySelector('img') || slide.querySelector('video');
+            if (mediaElement) {
+                let clonedMediaElement = mediaElement.cloneNode(true);
+                newItem.appendChild(clonedMediaElement);
+            }    
+            captions3Mb.appendChild(newItem);
+        });
+    }
+        
+    if (carousel == document.getElementById("carousel4") && captions4Mb && window.innerWidth <= 768) {
+        slides.forEach((slide, index) => {
+            console.log(slide)
+            const newItem = document.createElement('div');
+            newItem.classList.add(captionMbClass); 
 
-      slides[currentIndex].classList.add("active");
-      captionsMb[currentIndex].classList.add("active");
+            const titleElement = document.createElement('h4');
+            titleElement.classList.add(mbTitleClass);
+            titleElement.innerHTML = captions[index].querySelector(`.${captionTitleClass}`).innerHTML;
+            newItem.appendChild(titleElement);
 
-      updateCaptions();
-  }
+            let mediaElement = slide.querySelector('img') || slide.querySelector('video');
+            if (mediaElement) {
+                let clonedMediaElement = mediaElement.cloneNode(true);
+                newItem.appendChild(clonedMediaElement);
+            }    
+            captions3Mb.appendChild(newItem);
+        });
+    }
 
-  function updateCaptions() {
-      captions.forEach((caption) => {
-          const img = caption.querySelector("img");
-          const hr = caption.querySelector("hr");
-          if (img) {
-              img.style.display = "none";
-          }
-          if (hr) {
-              hr.style.backgroundColor = "#999";
-          }
-      });
+    function showSlide(index) {
+        if (index >= slides.length) {
+            currentIndex = 0;
+        } else if (index < 0) {
+            currentIndex = slides.length - 1;
+        } else {
+            currentIndex = index;
+        }
 
-      const activeCaptionImg = captions[currentIndex].querySelector("img");
-      const activeCaptionTitle = captions[currentIndex].querySelector(`.${captionTitleClass}`);
-      const activeCaptionDesc = captions[currentIndex].querySelector(`.${captionDescClass}`);
-      const activeCaptionHr = captions[currentIndex].querySelector(`.${captionHrClass}`);
+        slides.forEach((slide) => slide.classList.remove("active"));
+        captionsMb.forEach((slide) => slide.classList.remove("active"));
 
-      if (activeCaptionImg) {
-          activeCaptionImg.style.display = "block";
-      }
-      if (activeCaptionHr) {
-          activeCaptionHr.style.backgroundColor = "#6e1013";
-      }
-      if (activeCaptionTitle && activeCaptionDesc) {
-          activeCaptionMbTitle.innerHTML = activeCaptionTitle.textContent;
-          activeCaptionMbDesc.innerHTML = activeCaptionDesc.textContent;
-      }
-  }
+        slides[currentIndex].classList.add("active");
+        if (captionsMb[currentIndex]) {
+            captionsMb[currentIndex].classList.add("active");
+        }
+        updateCaptions();
+    }
 
-  function startCarousel() {
-      intervalId = setInterval(() => {
-          currentIndex++;
-          showSlide(currentIndex);
-      }, 3000);
-  }
+    function updateCaptions() {
+        captions.forEach((caption) => {
+            const img = caption.querySelector("img");
+            const hr = caption.querySelector("hr");
+            if (img) {
+                img.style.display = "none";
+            }
+            if (hr) {
+                hr.style.backgroundColor = "#999";
+            }
+        });
 
-  function stopCarousel() {
-      clearInterval(intervalId);
-  }
+        const activeCaptionImg = captions[currentIndex].querySelector("img");
+        const activeCaptionTitle = captions[currentIndex].querySelector(`.${captionTitleClass}`);
+        const activeCaptionDesc = captions[currentIndex].querySelector(`.${captionDescClass}`);
+        const activeCaptionHr = captions[currentIndex].querySelector(`.${captionHrClass}`);
+        let captions3Mb = document.getElementById("captions3Mb");
 
-  captions.forEach((caption, index) => {
-      caption.addEventListener("mouseover", () => {
-          stopCarousel();
-          showSlide(index);
-      });
-      caption.addEventListener("mouseout", () => {
-          startCarousel();
-      });
-      caption.addEventListener("click", () => {
-          showSlide(index);
-      });
-  });
+        if (activeCaptionImg) {
+            activeCaptionImg.style.display = "block";
+        }
+        if (activeCaptionHr) {
+            activeCaptionHr.style.backgroundColor = "#6e1013";
+        }
+        if (activeCaptionTitle && activeCaptionDesc) {
+            if (activeCaptionMbTitle && activeCaptionMbDesc) {
+                activeCaptionMbTitle.innerHTML = activeCaptionTitle.textContent;
+                activeCaptionMbDesc.innerHTML = activeCaptionDesc.textContent;
+            }
+        }
+    }
 
-  captionsMb.forEach((caption, index) => {
-      caption.addEventListener("mouseover", () => {
-          stopCarousel();
-          showSlide(index);
-      });
-      caption.addEventListener("mouseout", () => {
-          startCarousel();
-      });
-      caption.addEventListener("click", () => {
-          showSlide(index);
-      });
-  });
+    function startCarousel() {
+        intervalId = setInterval(() => {
+            currentIndex++;
+            showSlide(currentIndex);
+        }, 3000);
+    }
 
-  showSlide(currentIndex);
-  startCarousel();
+    function stopCarousel() {
+        clearInterval(intervalId);
+    }
+
+    captions.forEach((caption, index) => {
+        caption.addEventListener("mouseover", () => {
+            stopCarousel();
+            showSlide(index);
+        });
+        caption.addEventListener("mouseout", () => {
+            startCarousel();
+        });
+        caption.addEventListener("click", () => {
+            showSlide(index);
+        });
+    });
+
+    captionsMb.forEach((caption, index) => {
+        caption.addEventListener("mouseover", () => {
+            stopCarousel();
+            showSlide(index);
+        });
+        caption.addEventListener("mouseout", () => {
+            startCarousel();
+        });
+        caption.addEventListener("click", () => {
+            showSlide(index);
+        });
+    });
+
+    
+    showSlide(currentIndex);
+    startCarousel();
 }
 
 createCarousel('carousel1', 'carousel1-item', 'caption1', 'captions1-mb__pagination-item', 'caption1-title', 'caption1-desc', 'caption1 hr', 'captions1-mb__title', 'captions1-mb__desc');
 createCarousel('carousel2', 'carousel2-item', 'caption2', 'captions2-mb__pagination-item', 'caption2-title', 'caption2-desc', 'caption2 hr', 'captions2-mb__title', 'captions2-mb__desc');
-createCarousel('carousel3', 'carousel3-item', 'caption3', 'captions3-mb__pagination-item', 'caption3-title', 'caption3-desc', 'caption3 hr', 'captions3-mb__title', 'captions3-mb__desc');
+createCarousel('carousel3', 'carousel3-item', 'caption3', 'captions3-mb__item', 'caption3-title', 'caption3-desc', 'caption3 hr', 'captions3-mb__title', 'captions3-mb__desc');
+createCarousel('carousel4', 'carousel4-item', 'caption4', 'captions4-mb__item', 'caption4-title', 'caption4-desc', 'caption4 hr', 'captions4-mb__title', 'captions4-mb__desc');
